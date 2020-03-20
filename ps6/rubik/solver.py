57fa2bba7bf9ca9bf292bcee68cec9926bbc6c1a
Aleksandr Efremov <topefremov@yandex.ru>
2020-03-20 12:34:28 +0300

    ps6 rubik's two ways BFS

--- ps6/rubik/solver.py
+++ ps6/rubik/solver.py
@@ -12,28 +12,51 @@ def shortest_path(start, end):
     if start == end:
         return []
     
-    level = set(start)
-    parent = { (start, None): None }
-    frontier = [(start, None)]
-    while frontier:
+    start_level = set(start)
+    end_level = set(end)
+    parent_from_start = { (start, None): None }
+    frontier_from_start = [(start, None)]
+    parent_from_end = { (end, None): None }
+    frontier_from_end = [(end, None)]
+    
+    for i in range(0, 7):
         next = []
-        for position in frontier:
+        for position in frontier_from_start:
             for neighbor in neighbors(position[0]):
-                if neighbor[0] not in level:
-                    level.add(neighbor[0])
-                    parent[neighbor] = position
+                if neighbor[0] not in start_level:
+                    start_level.add(neighbor[0])
+                    parent_from_start[neighbor] = position
                     next.append(neighbor)
-                if neighbor[0] == end:
-                    return construct_path(neighbor, parent)             
-        frontier = next
+                    if neighbor[0] == end:
+                        return construct_path(neighbor, parent_from_start)    
+        frontier_from_start = next
+        next = []
+        for position in frontier_from_end:
+            for neighbor in neighbors(position[0]):
+                if neighbor[0] not in end_level:
+                    end_level.add(neighbor[0])
+                    parent_from_end[neighbor] = position
+                    next.append(neighbor)
+        frontier_from_end = next
+        intersection = set([el[0] for el in frontier_from_start]).intersection([el[0] for el in frontier_from_end])
+        if intersection:
+            conf = intersection.pop()
+            start_end = _find_conf(conf, frontier_from_start)
+            end_end = _find_conf(conf, frontier_from_end)
+            return construct_path_two_ends(start_end, parent_from_start, end_end, parent_from_end)
+        
                 
-                        
+def _find_conf(conf, frontier):
+    for el in frontier:
+        if el[0] == conf:
+            return el                    
 
 def neighbors(position):
     neighbors = []
     for twist in rubik.quarter_twists:
         neighbors.append((rubik.perm_apply(twist, position), twist))
     return neighbors     
+
 
 def construct_path(end, parent):
     position = end
@@ -42,4 +65,19 @@ def construct_path(end, parent):
         result.append(position[1])
         position = parent[position]
     result.reverse()
+    return result
+
+def construct_path_two_ends(start_end, parent_from_start, end_end, parent_from_end):
+    position = start_end
+    result = []
+    while parent_from_start[position] is not None:
+        result.append(position[1])
+        position = parent_from_start[position]
+    result.reverse()
+    
+    position = end_end
+    while parent_from_end[position] is not None:
+        result.append(rubik.perm_inverse(position[1]))
+        position = parent_from_end[position]
+    
     return result
